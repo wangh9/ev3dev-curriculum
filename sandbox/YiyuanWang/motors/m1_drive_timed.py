db440b9a01bbbdc8534f63857f7b13277aef652f
wangh9 <wangh9@rose-hulman.edu>
2017-10-25 13:11:32 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	sandbox/README.md

--- sandbox/YiyuanWang/motors/m1_drive_timed.py
+++ sandbox/YiyuanWang/motors/m1_drive_timed.py
@@ -117,6 +117,6 @@ ev3.Sound.speak("Goodbye").wait()
 #   400 dps 36 inches (make sure it drives within 9 inches of the target distance)
 # Do more tests if you see fit.  Ideally you should be +/- 25% of the target goal.
 #
-# TODO: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
+# DONE: 8. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 #  Observation you should make, the pattern run_forever-->time.sleep-->stop naturally blocks code execution until done.
