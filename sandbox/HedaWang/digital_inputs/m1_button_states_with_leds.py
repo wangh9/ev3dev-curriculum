a44ab896a6dc0c0e7853e9b8357fe0a3a345f526
wangh9 <wangh9@rose-hulman.edu>
2017-10-29 17:05:37 -0400

    finish the m1 and m2 in digital_inputs

--- sandbox/HedaWang/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/HedaWang/digital_inputs/m1_button_states_with_leds.py
@@ -5,10 +5,10 @@ This module lets you practice using the buttons on the EV3 as states.
 Normally we'll use event callbacks with buttons, but this example uses buttons as states for the purposes of example.
 Much like we did in HedaWang of the motors unit, later we will show you different (better) ways to use buttons.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Heda Wang.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
+# DONE: 2. Have someone on your team run this program as is on the EV3 and make sure everyone understands the code.
 #   You will exit the program by pressing the back button on the EV3 brick (button just below the screen).
 #   The back button is already implemented to exit the program (as you can see in the code below).
 
@@ -52,26 +52,61 @@ def main():
                   ev3.Leds.AMBER]
 
     current_color_index = 0
-    while True:
-        # TODO: 3. Implement the left, right, and up buttons as follows:
-        #    When the up button is being pressed:
-        #      -- print the word "up"
-        #      -- turn off all LEDs
-        #    When the left button is being pressed:
-        #      -- print the word "left"
-        #      -- make the LEFT led GREEN
-        #      -- turn off the right LED
-        #    When the right button is being pressed:
-        #      -- print "right"
-        #      -- make the RIGHT led RED
-        #      -- turn off the left LED
-        #   You are required to use the Button instance variables for the up, left, and right (not event callbacks).
-        #   Notice that the word "up" (or "left" or "right" is printed continually while you hold the button)
-        #   Optional: You can also comment out the code above that does the 6 second red, green, off pattern.  It was
-        #     there just to provide you with code examples for using the LEDs.  It does not need to run anymore.
-        #     Just make sure not to comment out too much. ;)
 
-        # TODO: 4. Implement the down button to change the color of both LEDs.
+# DONE: 3. Implement the left, right, and up buttons as follows:
+#    When the up button is being pressed:
+#      -- print the word "up"
+#      -- turn off all LEDs
+#    When the left button is being pressed:
+#      -- print the word "left"
+#      -- make the LEFT led GREEN
+#      -- turn off the right LED
+#    When the right button is being pressed:
+#      -- print "right"
+#      -- make the RIGHT led RED
+#      -- turn off the left LED
+#   You are required to use the Button instance variables for the up, left, and right (not event callbacks).
+#   Notice that the word "up" (or "left" or "right" is printed continually while you hold the button)
+#   Optional: You can also comment out the code above that does the 6 second red, green, off pattern.  It was
+#     there just to provide you with code examples for using the LEDs.  It does not need to run anymore.
+#     Just make sure not to comment out too much. ;)
+    state = 0
+    while True:
+        btn = ev3.Button()
+        if btn.left:
+            print("Left")
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+        elif btn.right:
+            print("Right")
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+        elif btn.up:
+            print("Up")
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+        elif btn.down:
+            print("down")
+            if state%4 == 0:
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+                time.sleep(0.1)
+            elif state%4 == 1:
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+                time.sleep(0.1)
+            elif state%4 == 2:
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+                time.sleep(0.1)
+            elif state%4 == 3:
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER)
+                time.sleep(0.1)
+            state = state + 1
+            time.sleep(0.01)
+
+        # DONE: 4. Implement the down button to change the color of both LEDs.
         #   The first press to down should make both LEDs GREEN, the next press makes them RED, then AMBER, then off.
         #   If the user presses the down button again, wrap around the list to GREEN and continue as before.
         #   If the user holds down the button, figure out how to make the color change still only happen once.
@@ -79,7 +114,7 @@ def main():
         #     with a while loop that blocks code execution until the down instance variable is False.
         #     Use a time.sleep(0.01) inside the while loop to do nothing but wait for the button to be released.
 
-        # TODO: 5. Formally test your work. When you think you have the problem complete run these tests:
+        # DONE: 5. Formally test your work. When you think you have the problem complete run these tests:
         #   Press Left - Green left LED is on (try holding the button down for a few seconds when you to the press)
         #   Press Right - Right right LED is on
         #   Press Up - Both LEDs are off
